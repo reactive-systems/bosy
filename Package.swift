c403be8eda71d9df953282c35e9831c3554f6ec9
Leander Tentrup <tentrup@react.uni-saarland.de>
2017-01-16 15:20:03 +0100

    change url of LTL library

--- Package.swift
+++ Package.swift
@@ -8,6 +8,6 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/ltentrup/CAiger.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://gitlab.com/ltentrup/LTL.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/ltentrup/LTL.git", majorVersion: 0, minor: 1),
     ]
 )
