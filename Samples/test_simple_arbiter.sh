9a801659dfa29db47307b3f6324e9f9a50e0f1b0
Leander Tentrup <tentrup@react.uni-saarland.de>
2017-06-29 12:48:40 +0200

    Add more output to test script

--- Samples/test_simple_arbiter.sh
+++ Samples/test_simple_arbiter.sh
@@ -1,11 +1,47 @@
 #!/usr/bin/env bash
 
+result=0
+
 # test realizability
-./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic 2>&1 | grep "result: realizable" || exit 1
-./bosy.sh Samples/simple_arbiter.bosy --backend explicit  2>&1 | grep "result: realizable" || exit 1
-./bosy.sh Samples/simple_arbiter.bosy --backend smt  2>&1 | grep "result: realizable" || exit 1
+echo "checking realizability using input-symbolic (QBF) backend"
+./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic 2>&1 | grep "result: realizable"
+if [ "$?" -ne "0" ]; then
+  result=1
+fi
+
+echo "checking realizability using explicit (SAT) backend"
+./bosy.sh Samples/simple_arbiter.bosy --backend explicit  2>&1 | grep "result: realizable"
+if [ "$?" -ne "0" ]; then
+  result=1
+fi
+
+echo "checking realizability using SMT backend"
+./bosy.sh Samples/simple_arbiter.bosy --backend smt  2>&1 | grep "result: realizable"
+if [ "$?" -ne "0" ]; then
+  result=1
+fi
 
 # test synthesis
-./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic --synthesize  2>&1 | grep "result: realizable" || exit 1
-./bosy.sh Samples/simple_arbiter.bosy --backend explicit --synthesize  2>&1 | grep "result: realizable" || exit 1
-./bosy.sh Samples/simple_arbiter.bosy --backend smt --synthesize 2>&1 | grep "result: realizable" || exit 1
+echo "synthesizing using input-symbolic (QBF) backend"
+./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic --synthesize  2>&1 | grep "result: realizable"
+if [ "$?" -ne "0" ]; then
+  result=1
+fi
+
+echo "synthesizing using explicit (SAT) backend"
+./bosy.sh Samples/simple_arbiter.bosy --backend explicit --synthesize  2>&1 | grep "result: realizable"
+if [ "$?" -ne "0" ]; then
+  result=1
+fi
+
+echo "synthesizing using SMT backend"
+./bosy.sh Samples/simple_arbiter.bosy --backend smt --synthesize 2>&1 | grep "result: realizable"
+if [ "$?" -ne "0" ]; then
+  result=1
+fi
+
+if [ "$result" -ne "0" ]; then
+  echo ""
+  echo "failed"
+fi
+exit $result
