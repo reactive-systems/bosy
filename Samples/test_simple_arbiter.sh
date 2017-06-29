2726c1724de86118648b3b3c6811dc53495121a1
Leander Tentrup <tentrup@react.uni-saarland.de>
2017-06-29 15:00:41 +0200

    Update test to have full tool output

--- Samples/test_simple_arbiter.sh
+++ Samples/test_simple_arbiter.sh
@@ -4,38 +4,38 @@ result=0
 
 # test realizability
 echo "checking realizability using input-symbolic (QBF) backend"
-./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic 2>&1 | grep "result: realizable"
+./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic
 if [ "$?" -ne "0" ]; then
   result=1
 fi
 
 echo "checking realizability using explicit (SAT) backend"
-./bosy.sh Samples/simple_arbiter.bosy --backend explicit  2>&1 | grep "result: realizable"
+./bosy.sh Samples/simple_arbiter.bosy --backend explicit
 if [ "$?" -ne "0" ]; then
   result=1
 fi
 
 echo "checking realizability using SMT backend"
-./bosy.sh Samples/simple_arbiter.bosy --backend smt  2>&1 | grep "result: realizable"
+./bosy.sh Samples/simple_arbiter.bosy --backend smt
 if [ "$?" -ne "0" ]; then
   result=1
 fi
 
 # test synthesis
 echo "synthesizing using input-symbolic (QBF) backend"
-./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic --synthesize  2>&1 | grep "result: realizable"
+./bosy.sh Samples/simple_arbiter.bosy --backend input-symbolic --synthesize
 if [ "$?" -ne "0" ]; then
   result=1
 fi
 
 echo "synthesizing using explicit (SAT) backend"
-./bosy.sh Samples/simple_arbiter.bosy --backend explicit --synthesize  2>&1 | grep "result: realizable"
+./bosy.sh Samples/simple_arbiter.bosy --backend explicit --synthesize
 if [ "$?" -ne "0" ]; then
   result=1
 fi
 
 echo "synthesizing using SMT backend"
-./bosy.sh Samples/simple_arbiter.bosy --backend smt --synthesize 2>&1 | grep "result: realizable"
+./bosy.sh Samples/simple_arbiter.bosy --backend smt --synthesize
 if [ "$?" -ne "0" ]; then
   result=1
 fi
