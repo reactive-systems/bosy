fd0c295e1c0a1b0bea493cc807fcf94cb8b18ecd
Leander Tentrup <tentrup@react.uni-saarland.de>
2017-12-18 12:36:31 +0100

    Add test case for unrealizable benchmark

--- Tests/BoundedSynthesisTests/DetectorUnrealizable.swift
+++ Tests/BoundedSynthesisTests/DetectorUnrealizable.swift
@@ -13,97 +13,25 @@ import CAiger
 
 @testable import BoundedSynthesis
 
-func modelCheckSMV(file: String) -> Bool {
-    let task = Process()
-    task.launchPath = "./Tools/NuSMV"
-    task.arguments = ["-keep_single_value_vars", file]
-    
-    let stdoutPipe = Pipe()
-    let stderrPipe = Pipe()
-    task.standardOutput = stdoutPipe
-    task.standardError = stderrPipe
-    task.launch()
-    
-    // there may be a large amount of stdout data
-    // by reading it before waiting, we avoid deadlocks
-    let stdoutHandle = stdoutPipe.fileHandleForReading
-    let stdoutData = StreamHelper.readAllAvailableData(from: stdoutHandle)
-    
-    task.waitUntilExit()
-    
-    guard let stdout = String(data: stdoutData, encoding: String.Encoding.utf8) else {
-        return false
-    }
-    
-    for line in stdout.components(separatedBy: "\n") {
-        if !line.hasPrefix("-- specification") {
-            continue
-        }
-        if !line.hasSuffix("is true") {
-            print(stdout)
-            return false
-        }
-        return true
-    }
-    
-    return false
-}
-
 /**
- * Tests if AIGER implementation satisfies given specification.
+ * Tests whether the synthesis result of the specification
  *
- * Transforms specification to empty SMV model with LTLSPEC, then uses the tool
- * `smvtoaig` from the AIGER toolset to build an AIGER monitor.
- * Then, we combine monitor and implementation and use `aigbmc` to search for
- * bounded counterexamples.
+ *     GF (r_0 && r_1) <-> GF g
+ *     G (r_0 && r_1) -> G (!g)
  *
- * TODO: should use non-bounded model checker instead.
+ * is unrealizable in all supported backends.
  */
-func modelCheckAiger(specification: SynthesisSpecification, implementation: UnsafeMutablePointer<aiger>) throws -> Bool {
-    guard let smvSecification = specification.smv else {
-        fatalError()
-    }
-    let tempFile = try TemporaryFile(suffix: ".smv")
-    let monitorFile = try TemporaryFile(suffix: ".aig")
-    let implementationFile = try TemporaryFile(suffix: ".aig")
-    let combinedFile = try TemporaryFile(suffix: ".aig")
+class DetectorUnrealizableTest: XCTestCase {
 
-    tempFile.fileHandle.write(Data(smvSecification.utf8))
+    let jsonSpec = "{\"semantics\": \"mealy\", \"inputs\": [\"r_0\", \"r_1\"], \"outputs\": [\"g\"], \"assumptions\": [], \"guarantees\": [\"((G ((F (r_0)) && (F (r_1)))) <-> (G (F (g))))\", \"(G (((r_0) && (r_1)) -> (G (! (g)))))\"] }"
 
-
-    try Basic.Process.checkNonZeroExit(arguments: ["./Tools/smvtoaig", "-L", "Tools/ltl2smv", tempFile.path.asString, monitorFile.path.asString])
-    
-    aiger_open_and_write_to_file(implementation, implementationFile.path.asString)
-
-    let combiner = Basic.Process(arguments: ["./Tools/combine-aiger", monitorFile.path.asString, implementationFile.path.asString])
-    try combiner.launch()
-    let combinationResult = try combiner.waitUntilExit()
-    let sequence: [UInt8] = try combinationResult.output.dematerialize().map(UInt8.init)
-
-    combinedFile.fileHandle.write(Data(bytes: sequence))
-
-    let modelCheckOutput = try Basic.Process.checkNonZeroExit(arguments: ["./Tools/aigbmc", combinedFile.path.asString])
-
-    if modelCheckOutput.hasPrefix("1") {
-        // found counterexample
-        print(modelCheckOutput)
-        return false
-    }
-    
-    return true
-}
-
-class SimpleArbiterTest: XCTestCase {
-    
-    let jsonSpec = "{\"semantics\": \"mealy\", \"inputs\": [\"r_0\", \"r_1\", \"r_2\"], \"outputs\": [\"g_0\", \"g_1\", \"g_2\"], \"assumptions\": [], \"guarantees\": [\"(G (((! (g_0)) && (! (g_1))) || (((! (g_0)) || (! (g_1))) && (! (g_2)))))\", \"(G ((r_0) -> (F (g_0))))\", \"(G ((r_1) -> (F (g_1))))\", \"(G ((r_2) -> (F (g_2))))\"] }"
-    
     var options = BoSyOptions()
-    
+
     // Realizability
     func testRealizabilityInputSymbolic() {
         options.solver = .rareqs
         options.qbfPreprocessor = .bloqqer
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -116,14 +44,12 @@ class SimpleArbiterTest: XCTestCase {
             return
         }
         var encoding = InputSymbolicEncoding(options: options, automaton: automaton, specification: specification, synthesize: false)
-        XCTAssertFalse(try encoding.solve(forBound: 2))
-        XCTAssertTrue(try encoding.solve(forBound: 3))
-        XCTAssertTrue(try encoding.solve(forBound: 4))
+        XCTAssertTrue(try encoding.solve(forBound: 1))
     }
-    
+
     func testRealizabilityExplicit() {
         options.solver = .picosat
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -136,14 +62,12 @@ class SimpleArbiterTest: XCTestCase {
             return
         }
         var encoding = ExplicitEncoding(options: options, automaton: automaton, specification: specification)
-        XCTAssertFalse(try encoding.solve(forBound: 2))
-        XCTAssertTrue(try encoding.solve(forBound: 3))
-        XCTAssertTrue(try encoding.solve(forBound: 4))
+        XCTAssertTrue(try encoding.solve(forBound: 1))
     }
-    
+
     func testRealizabilitySmt() {
         options.solver = .z3
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -156,13 +80,11 @@ class SimpleArbiterTest: XCTestCase {
             return
         }
         var encoding = SmtEncoding(options: options, automaton: automaton, specification: specification)
-        XCTAssertFalse(try encoding.solve(forBound: 2))
-        XCTAssertTrue(try encoding.solve(forBound: 3))
-        XCTAssertTrue(try encoding.solve(forBound: 4))
+        XCTAssertTrue(try encoding.solve(forBound: 1))
     }
-    
+
     func testRealizabilityGameSolver() {
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -177,12 +99,12 @@ class SimpleArbiterTest: XCTestCase {
         let encoding = SafetyGameReduction(options: options, automaton: automaton, specification: specification)
         XCTAssertTrue(try encoding.solve(forBound: 1))
     }
-    
+
     func testSynthesisInputSymbolic() throws {
         options.solver = .rareqs
         options.qbfPreprocessor = .bloqqer
         options.qbfCertifier = .quabs
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -195,12 +117,12 @@ class SimpleArbiterTest: XCTestCase {
             return
         }
         var encoding = InputSymbolicEncoding(options: options, automaton: automaton, specification: specification, synthesize: true)
-        XCTAssertTrue(try encoding.solve(forBound: 3))
+        XCTAssertTrue(try encoding.solve(forBound: 1))
         guard let transitionSystem = encoding.extractSolution() else {
             XCTFail()
             return
         }
-        
+
         // Check SMV implementation
         guard let smvRepresentation = (transitionSystem as? SmvRepresentable)?.smv else {
             XCTFail()
@@ -209,7 +131,7 @@ class SimpleArbiterTest: XCTestCase {
         let tempFile = try TemporaryFile(suffix: ".smv")
         tempFile.fileHandle.write(Data(smvRepresentation.utf8))
         XCTAssertTrue(modelCheckSMV(file: tempFile.path.asString))
-        
+
         // Check AIGER implementation
         guard let aigerRepresentation = (transitionSystem as? AigerRepresentable)?.aiger else {
             XCTFail()
@@ -217,10 +139,10 @@ class SimpleArbiterTest: XCTestCase {
         }
         XCTAssertTrue(try modelCheckAiger(specification: specification, implementation: aigerRepresentation))
     }
-    
+
     func testSynthesisExplicit() throws {
         options.solver = .picosat
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -233,12 +155,12 @@ class SimpleArbiterTest: XCTestCase {
             return
         }
         var encoding = ExplicitEncoding(options: options, automaton: automaton, specification: specification)
-        XCTAssertTrue(try encoding.solve(forBound: 3))
+        XCTAssertTrue(try encoding.solve(forBound: 1))
         guard let transitionSystem = encoding.extractSolution() else {
             XCTFail()
             return
         }
-        
+
         // Check SMV implementation
         guard let smvRepresentation = (transitionSystem as? SmvRepresentable)?.smv else {
             XCTFail()
@@ -247,7 +169,7 @@ class SimpleArbiterTest: XCTestCase {
         let tempFile = try TemporaryFile(suffix: ".smv")
         tempFile.fileHandle.write(Data(smvRepresentation.utf8))
         XCTAssertTrue(modelCheckSMV(file: tempFile.path.asString))
-        
+
         // Check AIGER implementation
         guard let aigerRepresentation = (transitionSystem as? AigerRepresentable)?.aiger else {
             XCTFail()
@@ -255,10 +177,10 @@ class SimpleArbiterTest: XCTestCase {
         }
         XCTAssertTrue(try modelCheckAiger(specification: specification, implementation: aigerRepresentation))
     }
-    
+
     func testSynthesisSmt() throws {
         options.solver = .z3
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -271,12 +193,12 @@ class SimpleArbiterTest: XCTestCase {
             return
         }
         var encoding = SmtEncoding(options: options, automaton: automaton, specification: specification)
-        XCTAssertTrue(try encoding.solve(forBound: 3))
+        XCTAssertTrue(try encoding.solve(forBound: 1))
         guard let transitionSystem = encoding.extractSolution() else {
             XCTFail()
             return
         }
-        
+
         // Check SMV implementation
         guard let smvRepresentation = (transitionSystem as? SmvRepresentable)?.smv else {
             XCTFail()
@@ -285,7 +207,7 @@ class SimpleArbiterTest: XCTestCase {
         let tempFile = try TemporaryFile(suffix: ".smv")
         tempFile.fileHandle.write(Data(smvRepresentation.utf8))
         XCTAssertTrue(modelCheckSMV(file: tempFile.path.asString))
-        
+
         // Check AIGER implementation
         guard let aigerRepresentation = (transitionSystem as? AigerRepresentable)?.aiger else {
             XCTFail()
@@ -293,9 +215,9 @@ class SimpleArbiterTest: XCTestCase {
         }
         XCTAssertTrue(try modelCheckAiger(specification: specification, implementation: aigerRepresentation))
     }
-    
+
     func testSynthesisGameSolver() {
-        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec)?.dualized else {
             XCTFail()
             return
         }
@@ -320,8 +242,8 @@ class SimpleArbiterTest: XCTestCase {
         }
         XCTAssertTrue(try modelCheckAiger(specification: specification, implementation: aigerRepresentation), "model checkig AIGER implementation failed")
     }
-    
-    static var allTests : [(String, (SimpleArbiterTest) -> () throws -> Void)] {
+
+    static var allTests : [(String, (DetectorUnrealizableTest) -> () throws -> Void)] {
         return [
             ("testRealizabilityInputSymbolic", testRealizabilityInputSymbolic),
             ("testRealizabilityExplicit", testRealizabilityExplicit),
@@ -334,3 +256,4 @@ class SimpleArbiterTest: XCTestCase {
         ]
     }
 }
+
