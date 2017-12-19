38bb61a2760164b2777fc28764c4417f27aecd4c
Leander Tentrup <tentrup@react.uni-saarland.de>
2017-12-19 10:49:18 +0100

    Add test case for HyperLTL synthesis

---
+++ Tests/BoundedSynthesisTests/HyperLTLSynthesis.swift
@@ -0,0 +1,73 @@
+
+import XCTest
+
+import Basic
+import Utility
+
+import Specification
+import LTL
+import Utils
+import TransitionSystem
+import Automata
+
+import CAiger
+
+@testable import BoundedSynthesis
+
+/**
+ * Test of HyperLTL synthesis backend.
+ * The specification is partitioned into `high` and `low` inputs/outputs with the
+ * non-interference requirement (`high` input does not influence `low` output):
+ *
+ *     forall pi1 pi2.( (o_l[pi1] <-> o_l[pi2]) W !(i_l[pi1] <-> i_l[pi2]) )
+ *
+ * Further, we have two disjunctive requirements:
+ *
+ *     (o_l[pi1] <-> i_h[pi1]) || (X o_l[pi1] <-> i_l[pi1])
+ *
+ * The first one can be satisfied with a system of size 1, but that violates
+ * the non-interference condition. Thus, a solution has to satisfy the second
+ * constraint that can be only satisfied with a system if size 2.
+ */
+class HyperLTLSynthesisTest: XCTestCase {
+
+    let jsonSpec = "{\"semantics\": \"mealy\", \"inputs\": [\"i_l\", \"i_h\"], \"outputs\": [\"o_l\"], \"assumptions\": [], \"guarantees\": [\"forall pi1 pi2. ( (o_l[pi1] <-> i_h[pi1]) || (X o_l[pi1] <-> i_l[pi1]) ) && ( (o_l[pi1] <-> o_l[pi2]) W !(i_l[pi1] <-> i_l[pi2]) )\"] }"
+
+    var options = BoSyOptions()
+
+    // Realizability
+
+    func testRealizabilitySmt() {
+        options.solver = .z3
+        guard let specification = SynthesisSpecification.fromJson(string: jsonSpec) else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(specification.assumptions.count, 0)
+        XCTAssertEqual(specification.guarantees.count, 1)
+        guard let hyperltl = specification.guarantees.first else {
+            XCTFail()
+            return
+        }
+        XCTAssert(hyperltl.isHyperLTL)
+        let body = hyperltl.ltlBody
+        guard let spot = (!body).spot else {
+            XCTFail()
+            return
+        }
+        guard let automaton = LTL2AutomatonConverter.spot.convert(ltl: spot) else {
+            XCTFail()
+            return
+        }
+        var encoding = HyperSmtEncoding(options: options, automaton: automaton, specification: specification)
+        XCTAssertFalse(try encoding.solve(forBound: 1))
+        XCTAssertTrue(try encoding.solve(forBound: 2))
+        XCTAssertTrue(try encoding.solve(forBound: 3))
+    }
+
+    static var allTests : [(String, (HyperLTLSynthesisTest) -> () throws -> Void)] {
+        return [
+            ("testRealizabilitySmt", testRealizabilitySmt),
+        ]
+    }
+}
