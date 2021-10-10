2bad0d853e1b023aa27a6a0d8f8a129b5bd96ed7
GamerPanda99 <hno3@pm.me>
2021-10-10 19:18:42 +0200

    add command checker and help

--- docker/run-docker.sh
+++ docker/run-docker.sh
@@ -21,41 +21,49 @@ if [ ${#@} -lt 2 ]; then
 	exit 0
 fi
 
+#find installed docker command
+if which podman; then
+	DOCKER_COMMAND=podman
+elif which docker; then
+	DOCKER_COMMAND=docker
+else
+	echo "Error: No container toolchain found" >&1
+	exit 1
+fi
 
 function get_abs_filename() {
- 	# $1 : relative filename
-  	echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+	# $1 : relative filename
+	echo "$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
 }
 
 command=$1
 command=${command,,}
 
+if [ "$command" == "build" ]; then
 
-if [ $command == "build" ]; then
-
-	if [ $(tr $2) == "full" ]; then
+	if [ "${2,,}" == "full" ]; then
 		$DOCKER_COMMAND build -t localhost/bosyrunner:full full
 		exit $?
 	fi
-	$DOCKER_COMMAND  build -t localhost/bosyrunner:default default
-exit $?
+	$DOCKER_COMMAND build -t localhost/bosyrunner:default default
+	exit $?
 fi
 
 #first param is run/ build, second is build type,
 # third is path, everything after that is bosy params
-if [ $command == "run" ]; then
+if [ "$command" == "run" ]; then
 	bosy_type=$2
 	mount_path=$3
 
-	if [ -f $mount_path ]; then
-		absfn=$(get_abs_filename $mount_path)
+	if [ -f "$mount_path" ]; then
+		absfn=$(get_abs_filename "$mount_path")
 		mount=$(dirname "$absfn")
 		file=$(basename "$absfn")
-		$DOCKER_COMMAND run -v $mount:/root/files:Z "localhost/bosyrunner:$bosy_type" /root/files/$file ${@:4}
+		$DOCKER_COMMAND run -v "$mount:/root/files:Z" "localhost/bosyrunner:$bosy_type" "/root/files/$file" "${@:4}"
 		exit $?
 	fi
 
-	$DOCKER_COMMAND run -v $mount_path:/root/files:Z localhost/bosyrunner:$bosy_type ${@:4}
+	$DOCKER_COMMAND run -v "$mount_path:/root/files:Z" "localhost/bosyrunner:$bosy_type" "${@:4}"
 
-exit $?
+	exit $?
 fi
